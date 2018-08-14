8acd284cac1a7c4f2deb7e774b03aa053692a20a
ArielFeingold <feingold.ariel@gmail.com>
2018-08-14 12:21:29 -0400

    added users controller. config routs set scope to API

---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_08_13_142951) do
+
+  create_table "exercises", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
+    t.integer "sets", default: 1
+    t.float "weight"
+    t.integer "user_id"
+    t.integer "workout_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stats", force: :cascade do |t|
+    t.float "wheight", default: 1.0
+    t.float "bodyfat", default: 1.0
+    t.float "shoulders", default: 1.0
+    t.float "chest", default: 1.0
+    t.float "left_bicep", default: 1.0
+    t.float "right_bicep", default: 1.0
+    t.float "right_thigh", default: 1.0
+    t.float "left_thigh", default: 1.0
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "workouts", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
